d631f80d91a142f30883cc424590c44719213856
Thanapat <t.veerakajorn@gmail.com>
2021-02-09 18:45:29 -0500

    manual submit

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+
+const CoordinatesButton = (props) => {
+    return (
+        <div>
+            <button onClick={e => props.onReceiveCoordinates([e.clientX, e.clientY])} >
+                Get Coordinates
+            </button>
+        </div>
+    )
+}
+
+export default CoordinatesButton;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,23 @@
 // Code DelayedButton Component Here
+import React from 'react';
+
+const DelayedButton = (props) => {
+
+    const onButtonClick = (event) => {
+        event.persist()
+
+        setTimeout(() => {
+            props.onDelayedClick(event)
+        }, props.delay)
+    }
+
+    return (
+        <div>
+            <button onClick={onButtonClick}>
+                Delayed Button
+            </button>
+        </div>
+    )
+}
+
+export default DelayedButton; 
